40503b6b25b3d590ed5b60aa4b71b5f4ed6b94a6
sleslie321 <scott@leslie.com>
2026-04-05 18:46:00 +0100

    claan up

--- web.holidaydo.mvc/Controllers/HomeController.cs
+++ web.holidaydo.mvc/Controllers/HomeController.cs
@@ -1,32 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using web.holidaydo.mvc.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace web.holidaydo.mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
