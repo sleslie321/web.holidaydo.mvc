8bb3dfed7495f618c9d1c791db1c07f3eaa665a0
sleslie321 <scott@leslie.com>
2026-04-05 11:57:27 +0100

    empty core web app

---
+++ web.holidaydo.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web.holidaydo.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
