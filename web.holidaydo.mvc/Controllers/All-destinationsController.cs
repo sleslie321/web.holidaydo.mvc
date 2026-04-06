730313eeb8efaaca9083fe282bb032c54ce784c2
sleslie321 <scott@leslie.com>
2026-04-06 16:41:33 +0100

    formating example for all-destinations

---
+++ web.holidaydo.mvc/Controllers/All-destinationsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.holidaydo.mvc.Controllers
+{
+    [Route("all-destinations")]
+    public class All_destinationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
