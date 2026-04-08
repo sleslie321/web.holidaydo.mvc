33caa51e696f960e1856c9370c7ff8d513b6df5c
sleslie321 <scott@leslie.com>
2026-04-08 22:21:10 +0100

    Add FlightFinder and travel books section
    Introduce FlightFinderController and its Index view to host a flight-finder widget. Add a travel books section to Destination/Index.cshtml with sample book data and UI. Tidy up Home/Index.cshtml layout (close markup and add placeholder tickets section). Bump holidaydo-theme.css version in _Layout.cshtml, fix Kenya destination link ID, and apply a minor CSS formatting/EOF fix for .city-link:hover.

---
+++ web.holidaydo.mvc/Controllers/FlightFinderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.holidaydo.mvc.Controllers
+{
+    public class FlightFinderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
